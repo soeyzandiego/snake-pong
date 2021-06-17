ad2f029e885fe08de41be35ce1a712d44037a998
soeyzandiego <59988129+soeyzandiego@users.noreply.github.com>
2021-06-16 23:02:58 -0500

    Movement + collision fixes
    Snake can't turn back on itself in opposite direction. When collecting the fruit, the snake bounces back in the direction of the last block of the snake (not the first). Game ends when snake hits the edge of the screen or itself

--- snake/snake.py
+++ snake/snake.py
@@ -9,7 +9,7 @@ cell_number = 25
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(10, 10), Vector2(9, 10), Vector2(8, 10)]
+        self.body = [Vector2(14, 10), Vector2(13, 10), Vector2(12, 10)]
         self.direction = Vector2(1, 0)
         self.blue = False
     
@@ -38,15 +38,20 @@ class SNAKE:
         self.body = body_copy
 
     def add_block(self):
-        self.body.insert(1, self.body[1] + self.direction)
+        self.body.insert(0, self.body[0] + self.direction)
 
     def subtract_block(self):
         self.body = self.body[:-1]
 
     def switch_head(self):
+        last_block = self.body[len(self.body) - 1]
+        second_last_block = self.body[len(self.body) - 2]
+
+        end_dir = second_last_block - last_block
+
         self.body.reverse()
         self.blue = not self.blue
-        self.direction *= -1
+        self.direction = end_dir * -1
           
 class RED_FRUIT:
     def __init__(self,):
@@ -75,19 +80,23 @@ class MAIN:
         self.snake = SNAKE()
         self.red_fruit = RED_FRUIT()
         self.blue_fruit = BLUE_FRUIT()
+        self.game_fin = False
 
         if self.blue_fruit.pos == self.red_fruit.pos:
             self.blue_fruit.respawn()
 
     def update(self):
+        if self.game_fin: return  # if game is over, don't update
+
         self.snake.move_snake()
         self.collision()
         self.check_lose()
 
     def draw(self):
-        self.snake.draw_snake()
         self.red_fruit.draw_fruit()
         self.blue_fruit.draw_fruit()
+        if self.game_fin: return  # if game is over, don't draw snake
+        self.snake.draw_snake()
 
     def collect_red(self):
         self.red_fruit.respawn()
@@ -114,12 +123,24 @@ class MAIN:
                 self.snake.subtract_block()
 
     def check_lose(self):
-        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+        if self.game_fin:
+            return
+        elif not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number: # if the snake head isn't between the display size, end the game
             self.game_over()
+        else:
+            for block in self.snake.body[1:]:
+                if block == self.snake.body[0]:
+                    self.game_over()
 
     def game_over(self):
-        pygame.quit()
-        sys.exit()
+        del self.snake  # delete the current snake
+        self.game_fin = True
+
+    def reset(self):
+        self.snake = SNAKE()
+        self.red_fruit.respawn()
+        self.blue_fruit.respawn()
+        self.game_fin = False
 
 pygame.init()
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
@@ -137,14 +158,17 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and game.snake.direction.x != 1:
                 game.snake.direction = Vector2(-1, 0)
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT and game.snake.direction.x != -1:
                 game.snake.direction = Vector2(1, 0)
-            elif event.key == pygame.K_UP:
-                game.snake.direction = Vector2(0, -1)
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_UP and game.snake.direction.y != 1:
+                    game.snake.direction = Vector2(0, -1)
+            elif event.key == pygame.K_DOWN and game.snake.direction.y != -1:
                 game.snake.direction = Vector2(0, 1)
+
+            if event.key == pygame.K_r and game.game_fin:
+                game.reset()
         if event.type == SCREEN_UPDATE:
             game.update()
 
